8942fccc3bb4ceba4a225acf2d1fc7b14b1f016c
Katarzyna Da Silva <conic916@gmail.com>
2019-06-19 10:57:40 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import DevTools from './devTools';
